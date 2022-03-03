4c80a63c1dcae0b6d33f42c16baf3d8a9a930e09
jiwonkim97 <jiwonkim97@naver.com>
2022-03-03 19:43:12 +0900

    Update mainPage.js

--- src/components/mainPage.js
+++ src/components/mainPage.js
@@ -1,5 +1,6 @@
 import { url } from '../modules/Url'
 import axios from 'axios'
+import { useState } from 'react';
 
 const getEvents = async () => {
   try {
@@ -30,7 +31,26 @@ const addEvent = async () => {
   }
 }
 
+
+
 function Main() {
+  const [eventKey, setEventKey] = useState(-1)
+
+  const deleteEvent = async () => {
+    //console.log(eventKey)
+    try {
+      const response = await axios.post(
+        `${url}/deleteEvent`,
+        {
+          key: eventKey
+        }
+      );
+      console.log(response.data)
+    } catch (e) {
+      console.log(e)
+    }
+  }
+
   return (
     <div>
       MainPage
@@ -39,6 +59,13 @@ function Main() {
       </button>
       <button onClick={addEvent}>
         addEvents
+      </button>
+      <input
+        onChange={(e) => setEventKey(e.target.value)}
+        placeholder='지울 이벤트 key 입력'
+        />
+      <button onClick={deleteEvent}>
+        deleteEvent
       </button>
     </div>
   )
