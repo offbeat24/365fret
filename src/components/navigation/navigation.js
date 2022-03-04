0a3b294ba67a172c9e9e9f963e51e1e598a08076
Jihan <parkjihan5253@gmail.com>
2022-03-04 12:59:47 +0900

    style: NavigationBar

--- src/components/navigation/navigation.js
+++ src/components/navigation/navigation.js
@@ -3,23 +3,21 @@ import Scheduler from './Scheduler.png';
 import Setting from './Setting.png';
 import styled from 'styled-components';
 import CssBaseline from '@mui/material/CssBaseline';
-import { useState } from 'react';
 
-function Navigation(){
-    const [ flag, setFlag ] = useState(1);
+function Navigation({ setFlag }){
     const clickIcon = (num) => {
         setFlag(num);
     };
     return(
         <Nav>
             <CssBaseline/>
-              <Logo>365fret</Logo>
+              <Logo onClick={() => clickIcon(1)}>365fret</Logo>
             <NavLinkBox>
-                <NavBtnImg onClick={clickIcon(1)} src={Main}/>
-                <NavBtnImg onClick={clickIcon(2)} src={Scheduler}/>
-                <NavBtnImg onClick={clickIcon(3)} src={Setting}/>
+                <NavBtnImg onClick={() => clickIcon(1)} src={Main}/>
+                <NavBtnImg onClick={() => clickIcon(2)} src={Scheduler}/>
+                <NavBtnImg onClick={() => clickIcon(3)} src={Setting}/>
                 <NavLink>
-                    <Circle>JH</Circle>
+                    <Circle onClick={() => clickIcon(4)}>JH</Circle>
                 </NavLink>
             </NavLinkBox>
         </Nav>
--- src/components/signedPages/settingPage.js
+++ src/components/signedPages/settingPage.js
@@ -1,9 +1,15 @@
-function Setting(){
-    return (
-        <div>
-            SettingPage
-        </div>
-    );
+import styled from 'styled-components';
+
+function Setting() {
+  return (
+    <Page>
+        환경설정페이지
+    </Page>
+);
 }
+
+const Page = styled.div`
+  margin: 10px;
+`
 
 export default Setting;
