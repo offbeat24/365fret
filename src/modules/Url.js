d35bf0152170b6236065cda9a4c3544b079bf620
offbeat24 <77655547+offbeat24@users.noreply.github.com>
2022-05-18 12:48:48 +0900

    zz

--- src/modules/Url.js
+++ src/modules/Url.js
@@ -1,5 +1,5 @@
 //export const url = 'http://210.113.51.164:3000'
-export const url = 'http://121.132.137.68:3000'
+export const url = 'http://192.168.1.105:3000'
 //export const url = 'http://172.20.10.2:3000'
 
 
